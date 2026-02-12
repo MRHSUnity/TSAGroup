6a164a608eead06efedf0fc37a3c697225b6fe26
codelover5 <k1525905@students.katyisd.org>
2026-02-12 13:38:39 -0600

    simplified animator

--- Assets/Scripts/Whip.cs
+++ Assets/Scripts/Whip.cs
@@ -13,23 +13,10 @@ public class Whip : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!anim.GetBool("isWalkingLeft") && !anim.GetBool("attackWhip"))
-        {
-            attackArea.transform.localPosition = new Vector3(.255f, attackArea.transform.localPosition.y, attackArea.transform.localPosition.z);
-        }
-        if (anim.GetBool("isWalkingLeft") && !anim.GetBool("attackWhip"))
-        {
-            attackArea.transform.localPosition = new Vector3(-.275f, attackArea.transform.localPosition.y, attackArea.transform.localPosition.z);
-        }
-    }
+
     public void attacky()
     {
-        
-       
         attackArea.SetActive(true);
-
     }
     
     public void endAttacky()
