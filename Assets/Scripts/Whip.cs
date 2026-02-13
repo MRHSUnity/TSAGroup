ae7824fcbb657afd088a7246cf330d77754efd01
shape-shiftR <K1902049@students.katyisd.org>
2026-02-13 13:22:01 -0600

    Player SFX (Whip SFX, READ BELOW!!
    Edited Whip Script to do so, which means it might clash with a tutorial push.
    Edited speicifically
    Whip (script)
    Player object (added an empty game object to it, in which I stored the audio)

--- Assets/Scripts/Whip.cs
+++ Assets/Scripts/Whip.cs
@@ -5,6 +5,7 @@ public class Whip : MonoBehaviour
     public GameObject attackArea;
     private Animator anim;
     public EnemyHealth enemy;
+    public AudioSource audioSource;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +17,7 @@ public class Whip : MonoBehaviour
 
     public void attacky()
     {
+        audioSource.Play();
         attackArea.SetActive(true);
     }
     
