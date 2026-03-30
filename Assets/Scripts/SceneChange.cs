25dada30ce40e6d635e5e73acd233d43274d690f
dakotahlm <136293530+dakotahlm@users.noreply.github.com>
2026-03-30 13:40:18 -0500

    scene 3

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,11 +5,21 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     public String Scene;
+    public LayerMask player;
  private void OnTriggerEnter2D(Collider2D other)
  {
 
          // Load the next scene
-         SceneManager.LoadScene(Scene);
+         if(player==null)
+        {
+            SceneManager.LoadScene(Scene);
 
- }
+        }
+        if (((1 << other.gameObject.layer) & player) != 0)
+        {
+                SceneManager.LoadScene(Scene);
+
+
+        }
+    }
 }
