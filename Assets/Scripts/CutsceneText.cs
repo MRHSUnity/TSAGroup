63f1cabc95102f540bb07f31ee2919106814f341
shape-shiftR <K1902049@students.katyisd.org>
2026-05-06 13:46:08 -0500

    Scene additions
    Made cutscenes to provide player with more story context

--- Assets/Scripts/CutsceneText.cs
+++ Assets/Scripts/CutsceneText.cs
@@ -1,16 +1,77 @@
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using System.Collections;
+using NUnit.Framework.Internal.Commands;
+using System;
+using UnityEngine.SceneManagement;
 
 public class CutsceneText : MonoBehaviour
 {
+
+
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpd;
+    private int index;
+    public String scene;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        index = 0;
+        //StartDialogue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
     }
+    public void StartDialogue()
+    {
+        textComponent.text = string.Empty;
+        StartCoroutine(TypeLine());
+        Debug.Log(lines[index]);
+
+    }
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpd);
+        }
+    }
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            SceneManager.LoadScene(scene);
+            gameObject.SetActive(false);
+        }
+    }
+    public void Stop()
+    {
+        gameObject.SetActive(false);
+    }
+
 }
